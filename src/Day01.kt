f2f6caf3b17f6c70d04072a1da80170ab2015e9a
Slawomir Bolimowski <slawomir.bolimowski@allegro.pl>
2021-12-02 00:08:52 +0100

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(lines: List<String>): Int {
+        var prev = Long.MAX_VALUE
+        var counter: Int = 0
+        lines.forEach { line ->
+            if (line.toLong() > prev)
+                counter++
+
+            prev = line.toLong()
+        }
+        return counter
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(lines: List<String>): Int {
+        var prev = Long.MAX_VALUE
+        var counter: Int = 0
+        for (i in 3..lines.size) {
+            val sum = lines[i-1].toLong() + lines[i-2].toLong() + lines[i-3].toLong()
+            if (sum > prev)
+                counter++
+
+            prev = sum
+        }
+        return counter
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+
+    val input2 = readInput("Day01_2")
+    println(part1(input2))
+    println(part2(input2))
 }
