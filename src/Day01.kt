c7b938f248a2b5fcdd8d6aa159f3459f47232711
Slawomir Bolimowski <slawomir.bolimowski@allegro.pl>
2021-12-02 08:16:17 +0100

    day02

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,39 +1,54 @@
 fun main() {
     fun part1(lines: List<String>): Int {
-        var prev = Long.MAX_VALUE
-        var counter: Int = 0
+        var horizontal = 0
+        var depth = 0
         lines.forEach { line ->
-            if (line.toLong() > prev)
-                counter++
-
-            prev = line.toLong()
+            val (operation, argument) = line.trim().split(" ")
+            when (operation) {
+                "forward" -> {
+                    horizontal += argument.toInt()
+                }
+                "down" -> {
+                    depth += argument.toInt()
+                }
+                "up" ->
+                    depth -= argument.toInt()
+            }
         }
-        return counter
+        return horizontal * depth
     }
 
-    fun part2(lines: List<String>): Int {
-        var prev = Long.MAX_VALUE
-        var counter: Int = 0
-        for (i in 3..lines.size) {
-            val sum = lines[i-1].toLong() + lines[i-2].toLong() + lines[i-3].toLong()
-            if (sum > prev)
-                counter++
-
-            prev = sum
+    fun part2(lines: List<String>): Long {
+        var horizontal: Long = 0
+        var depth: Long = 0
+        var aim: Long = 0
+        lines.forEach { line ->
+            val (operation, argument) = line.trim().split(" ")
+            when (operation) {
+                "forward" -> {
+                    horizontal += argument.toLong()
+                    depth += aim * argument.toLong()
+                }
+                "down" -> {
+                    aim += argument.toLong()
+                }
+                "up" ->
+                    aim -= argument.toLong()
+            }
         }
-        return counter
+        return horizontal * depth
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 7)
-    check(part2(testInput) == 5)
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900L)
 
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
 
-    val input2 = readInput("Day01_2")
+    val input2 = readInput("Day02_2")
     println(part1(input2))
     println(part2(input2))
 }
