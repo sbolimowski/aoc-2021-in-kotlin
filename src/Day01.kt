65b27d0e8cd060158bcd20827d133532ac250075
Slawomir Bolimowski <slawomir.bolimowski@allegro.pl>
2021-12-03 11:34:23 +0100

    day03

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,54 +1,39 @@
 fun main() {
     fun part1(lines: List<String>): Int {
-        var horizontal = 0
-        var depth = 0
+        var prev = Long.MAX_VALUE
+        var counter: Int = 0
         lines.forEach { line ->
-            val (operation, argument) = line.trim().split(" ")
-            when (operation) {
-                "forward" -> {
-                    horizontal += argument.toInt()
-                }
-                "down" -> {
-                    depth += argument.toInt()
-                }
-                "up" ->
-                    depth -= argument.toInt()
-            }
+            if (line.toLong() > prev)
+                counter++
+
+            prev = line.toLong()
         }
-        return horizontal * depth
+        return counter
     }
 
-    fun part2(lines: List<String>): Long {
-        var horizontal: Long = 0
-        var depth: Long = 0
-        var aim: Long = 0
-        lines.forEach { line ->
-            val (operation, argument) = line.trim().split(" ")
-            when (operation) {
-                "forward" -> {
-                    horizontal += argument.toLong()
-                    depth += aim * argument.toLong()
-                }
-                "down" -> {
-                    aim += argument.toLong()
-                }
-                "up" ->
-                    aim -= argument.toLong()
-            }
+    fun part2(lines: List<String>): Int {
+        var prev = Long.MAX_VALUE
+        var counter: Int = 0
+        for (i in 3..lines.size) {
+            val sum = lines[i-1].toLong() + lines[i-2].toLong() + lines[i-3].toLong()
+            if (sum > prev)
+                counter++
+
+            prev = sum
         }
-        return horizontal * depth
+        return counter
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day02_test")
-    check(part1(testInput) == 150)
-    check(part2(testInput) == 900L)
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day02")
+    val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 
-    val input2 = readInput("Day02_2")
+    val input2 = readInput("Day01_2")
     println(part1(input2))
     println(part2(input2))
 }
---
+++ src/Day03.kt
@@ -0,0 +1,65 @@
+fun main() {
+
+    fun part1(lines: List<String>): Int {
+
+
+        val tmp = lines
+            .map { it.map { c -> c.toString().toInt() } }
+            .reduce { acc: List<Int>, bytes -> acc.zip(bytes) { a, b -> a + b } }
+            .map { if (it > lines.size / 2) 1 else 0 }
+
+        val gamma = tmp
+            .joinToString(separator = "")
+            .toInt(2)
+        //println(gamma.toString(2))
+
+        val epsilon = tmp
+            .map { if (it == 1) 0 else 1 }
+            .joinToString(separator = "")
+            .toInt(2)
+        //println(epsilon.toString(2))
+
+        return gamma * epsilon
+    }
+
+    fun part2(lines: List<String>): Int {
+        val values = lines.map { it.map { c -> c.toString().toInt() } }
+
+        var o2 = values
+        var co2 = values
+
+        for (i in 0..12) {
+
+            if (o2.size > 1) {
+                val mostCommon = if (o2.count { it[i] == 1 } >= o2.count { it[i] == 0 }) 1 else 0
+                o2 = o2.filter { it[i] == mostCommon }
+            }
+
+
+            if (co2.size > 1) {
+                val leastCommon = if (co2.count { it[i] == 1 } >= co2.count { it[i] == 0 }) 0 else 1
+                co2 = co2.filter { it[i] == leastCommon }
+            }
+
+            if (o2.size == 1 && co2.size == 1) break
+        }
+
+       val o2rating = o2.first().joinToString(separator = "").toInt(2)
+       val co2rating = co2.first().joinToString(separator = "").toInt(2)
+
+        return o2rating * co2rating
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 198)
+    check(part2(testInput) == 230)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+
+    val input2 = readInput("Day03_2")
+    println(part1(input2))
+    println(part2(input2))
+}
